f0b0bef458a8c0c05e8439e8f04ad73e4ee6297d
InshiyaRavat <96529913+InshiyaRavat@users.noreply.github.com>
2025-11-07 07:01:01 +0530

    Create optimal_backtracking.java

---
+++ CombinationSum/optimal_backtracking.java
@@ -0,0 +1,24 @@
+class Solution {
+    private void backtrack(int[] candidates, int start, int remaining, List<Integer> current, List<List<Integer>> result){
+        if(remaining == 0){
+            result.add(new ArrayList<>(current));
+            return;
+        }
+
+        for(int i= start; i< candidates.length; i++){
+            if(remaining < candidates[i]){
+                break;
+            }
+            current.add(candidates[i]);
+            backtrack(candidates, i, remaining - candidates[i], current, result);
+            current.remove(current.size() -1);
+        }
+    }
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        Arrays.sort(candidates);
+        backtrack(candidates, 0, target, new ArrayList<>(), result);
+        return result;
+    }
+}
