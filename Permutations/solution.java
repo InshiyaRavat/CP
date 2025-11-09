c61ba29ab5d847bca285c9bb46dac40fe63cb53d
InshiyaRavat <96529913+InshiyaRavat@users.noreply.github.com>
2025-11-09 07:36:27 +0530

    Create solution.java

---
+++ Permutations/solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    private void backtrack(int[] nums, List<List<Integer>> result, List<Integer> current, boolean[] used){
+        if(nums.length == current.size()){
+            result.add(new ArrayList<>(current));
+            return;
+        }
+
+        for(int i=0;i<nums.length;i++){
+            if(!used[i]){
+                used[i] = true;
+                current.add(nums[i]);
+                backtrack(nums, result, current, used);
+                current.remove(current.size() -1);
+                used[i] = false;
+            }
+        }
+    }
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        boolean[] used = new boolean[nums.length];
+
+        backtrack(nums, result, new ArrayList<>(), used);
+
+        return result;
+    }
+}
